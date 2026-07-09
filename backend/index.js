887abfe65bbbff9c4045a6bca9772c68199dbd59
Aditya-RS09 <158597529+Aditya-RS09@users.noreply.github.com>
2026-07-10 02:51:38 +0530

    push new code

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,17 @@
+require ('dotenv').config();
+
 const express = require("express");
+const mongoose = require("mongoose");
+
+
+const PORT =process.env.PORT || 3002;
+const uri =process.env.MONGO_URL || 3002
+
 
 const app = express();
 
-app.listen(3002, ()=>{
+
+app.listen(PORT, ()=>{
     console.log("App stated")
-})
+    mongoose.connect(uri);
+});
